efa415f338d8afc21a5e42c9c273aed75d7b7f9a
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-04-18 21:00:38 +0530

    feat: main nav menu

--- nse-app/src/components/header/Header.tsx
+++ nse-app/src/components/header/Header.tsx
@@ -1,10 +1,16 @@
 import ThemeToggle from "../ThemeToggle";
-import MainNav from "./MainNav";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import MainNavigationMenu from "./MainNavigationMenu";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between px-5">
-      <MainNav></MainNav>
+    <header className="flex items-center justify-between px-5 h-20">
+      <Avatar className="border p-1">
+        <AvatarImage src="/logo.svg"></AvatarImage>
+        <AvatarFallback>NSE</AvatarFallback>
+      </Avatar>
+
+      <MainNavigationMenu></MainNavigationMenu>
       <ThemeToggle></ThemeToggle>
     </header>
   );
