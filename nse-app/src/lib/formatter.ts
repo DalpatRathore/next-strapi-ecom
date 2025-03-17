782f112ffa32de7e7ab0c5acfa6c1cedaba28703
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2025-03-17 17:50:57 +0530

    fix: deleted old nse-app

--- nse-app/src/lib/formatter.ts
+++
@@ -1,8 +0,0 @@
-const DATE_TIME_FOMATTER = new Intl.DateTimeFormat("en", {
-  dateStyle: "medium",
-  timeStyle: "short",
-});
-
-export const formatDateTime = (date: Date) => {
-  return DATE_TIME_FOMATTER.format(date);
-};
